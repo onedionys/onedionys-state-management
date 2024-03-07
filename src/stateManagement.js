f58ea996e7522ce31582a4ca803c49d882a75dc3
onedionys <onedionys@gmail.com>
2024-03-07 11:07:55 +0700

    chore: initialize function project

---
+++ src/stateManagement.js
@@ -0,0 +1,31 @@
+class StateManager {
+    constructor(initialState = {}) {
+        this.state = initialState;
+        this.listeners = [];
+    }
+
+    getState() {
+        return this.state;
+    }
+
+    setState(newState) {
+        this.state = { ...this.state, ...newState };
+        this.notifyListeners();
+    }
+
+    addListener(listener) {
+        this.listeners.push(listener);
+    }
+
+    removeListener(listener) {
+        this.listeners = this.listeners.filter(l => l !== listener);
+    }
+
+    notifyListeners() {
+        this.listeners.forEach(listener => {
+            listener(this.state);
+        });
+    }
+}
+
+module.exports = StateManager;
